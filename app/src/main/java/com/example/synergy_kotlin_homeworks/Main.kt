487c69d294017280fdc737f47d577d50281ae99a
Oladiya <baymyxy@gmail.com>
2023-12-21 12:49:30 +0300

    feat: add module 2 homework 5

--- app/src/main/java/com/example/synergy_kotlin_homeworks/Main.kt
+++ app/src/main/java/com/example/synergy_kotlin_homeworks/Main.kt
@@ -1,83 +1,9 @@
 package com.example.synergy_kotlin_homeworks
 
-import java.math.RoundingMode
-
 fun main() {
-
-    val allHomeworks: Array<Double> = arrayOf(
-        2.1,
-        2.2,
-        2.3,
-        2.4,
-    )
-
-    var lastModuleNumber: Int? = null
-    allHomeworks.forEach {
-        val moduleNumber = getModuleNumber(it)
-        val homeworkNumber = getHomeworkNumber(it)
-
-        if ((lastModuleNumber == null) or (moduleNumber != lastModuleNumber)) {
-            println("Модуль $moduleNumber\n")
-        }
-        println("Домашняя работа $homeworkNumber")
-
-        print("Вывод:\n")
-        callModule(moduleNumber, homeworkNumber)
-        println()
-
-        lastModuleNumber = moduleNumber
-    }
-}
-
-fun getModuleNumber(double: Double): Int {
-    return double.toBigDecimal().setScale(1, RoundingMode.FLOOR).toInt()
-}
-
-fun getHomeworkNumber(double: Double): Int {
-    return (double * 10 % 10).toInt()
-}
-
-fun callModule(moduleNumber: Int, homeworkNumber: Int) {
-    when (moduleNumber) {
-        2 -> module2(homeworkNumber)
-    }
-}
-
-fun module2(homeworkNumber: Int) {
-    when (homeworkNumber) {
-        1 -> module2_homework1()
-        2 -> module2_homework2()
-        3 -> module2_homework3()
-        4 -> module2_homework4()
-    }
-}
-
-fun module2_homework1() {
 
     var x = 1
 
     while (x <= 3) println("$x в квадрате равно ${x * x++}")
-
-}
-
-fun module2_homework2() {
-
-    for (n in 1..7) println("Значение переменной \"n\" равно $n")
-
-}
-
-fun module2_homework3() {
-
-        var x = 1
-
-        while (x <= 6) println("Значение переменной \"x\" равно ${x++}")
-
-}
-
-fun module2_homework4(){
-
-    var y = 5
-
-    while (y >= 2) println("Значение переменной \"y\" равно ${y--}")
 
 }
